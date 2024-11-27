17ddae3f933c2876cae6268a215099578f1a9e40
Andrew Lombardo <150087831+Levalvus@users.noreply.github.com>
2024-11-27 18:38:35 -0500

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,7 +8,12 @@ function Contact() {
       <h1 className='mt-3 mb-3'>Contact Us</h1>
       <div className="row mb-5">
         <div className="col-md-5 mb-3">
-          <div>Text Goes Here</div>
+          <div>eatsomemore@marist.edu (203) 999-9999</div>
+          <div>## Street Name</div>
+          <div>Hours:</div>
+          <div>Monday - Friday: 12 - 11 </div>
+          <div>Saturday: 10 - 11</div>
+          <div>Sunday: 2 - 10</div>
         </div>
         <div className="col-md-5 text-center">
           <img src='https://mdbootstrap.com/img/new/standard/nature/184.webp' alt='...' className="map-img"/>
