6efd4b350fd441b6ab44cc7283a857c50aae0327
Andrew Lombardo <150087831+Levalvus@users.noreply.github.com>
2024-11-27 18:25:38 -0500

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,8 +11,28 @@ function About() {
           <img src='https://mdbootstrap.com/img/new/standard/nature/184.webp' alt='...' className="about-img"/>
         </div>
         <div class="col-md-8 mb-3">
-          <h3>Person Name</h3>
-          <h5>Person Title</h5>
+          <h3>Sophia Masone</h3>
+          <h5><ul>Documentation</ul><ul>Programmer</ul></h5>
+          <p>Chill Person</p>
+        </div>
+      </div>
+      <div class="row mb-5">
+        <div class="col-md-4 mb-3">
+          <img src='https://mdbootstrap.com/img/new/standard/nature/184.webp' alt='...' className="about-img"/>
+        </div>
+        <div class="col-md-8 mb-3">
+          <h3>Lainey Wright</h3>
+          <h5><ul>Framework Designer</ul><ul>Back-end Programmer</ul></h5>
+          <p>Chill Person</p>
+        </div>
+      </div>
+      <div class="row mb-5">
+        <div class="col-md-4 mb-3">
+          <img src='https://mdbootstrap.com/img/new/standard/nature/184.webp' alt='...' className="about-img"/>
+        </div>
+        <div class="col-md-8 mb-3">
+          <h3>Andrew Lombardo</h3>
+          <h5><ul>Front-end Programmer</ul><ul>Trello Manager</ul></h5>
           <p>Bio of person</p>
         </div>
       </div>
@@ -21,28 +41,8 @@ function About() {
           <img src='https://mdbootstrap.com/img/new/standard/nature/184.webp' alt='...' className="about-img"/>
         </div>
         <div class="col-md-8 mb-3">
-          <h3>Person Name</h3>
-          <h5>Person Title</h5>
-          <p>Bio of person</p>
-        </div>
-      </div>
-      <div class="row mb-5">
-        <div class="col-md-4 mb-3">
-          <img src='https://mdbootstrap.com/img/new/standard/nature/184.webp' alt='...' className="about-img"/>
-        </div>
-        <div class="col-md-8 mb-3">
-          <h3>Person Name</h3>
-          <h5>Person Title</h5>
-          <p>Bio of person</p>
-        </div>
-      </div>
-      <div class="row mb-5">
-        <div class="col-md-4 mb-3">
-          <img src='https://mdbootstrap.com/img/new/standard/nature/184.webp' alt='...' className="about-img"/>
-        </div>
-        <div class="col-md-8 mb-3">
-          <h3>Person Name</h3>
-          <h5>Person Title</h5>
+          <h3>Christopher Castillo</h3>
+          <h5><ul>Lead Graphic Design</ul><ul>Reservation Programmer</ul></h5>
           <p>Bio of person</p>
         </div>
       </div>
